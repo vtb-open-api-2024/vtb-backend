76caec285eb3c46e7a4daaefdd964f90fae621e7
pendulum <rs.pendulum@gmail.com>
2024-11-06 14:12:01 +0300

    moved to nginx

--- src/main.ts
+++ src/main.ts
@@ -3,17 +3,10 @@ import { AppModule } from './modules/app/app.module';
 import { CONFIG_APP } from './config/config.export';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
-import { readFileSync } from 'fs';
-import { join } from 'path';
 
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    // httpsOptions: {
-    //   key: readFileSync(join('ssl/certificate.key')).toString(),
-    //   cert: readFileSync(join('ssl/fullchain.crt')).toString()
-    // },
-  });
+  const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
