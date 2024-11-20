4e27a57e8d9210b80f90be68ade7beb54db280fa
pendulum <rs.pendulum@gmail.com>
2024-11-20 20:44:30 +0300

    fixed infinity

--- src/modules/utilities/transactional.decorator.ts
+++ src/modules/utilities/transactional.decorator.ts
@@ -1,4 +1,4 @@
-import { ethers } from 'ethers';
+import 'reflect-metadata';
 import { DataSource, Repository, QueryRunner } from 'typeorm';
 import { IsolationLevel } from 'typeorm/driver/types/IsolationLevel';
 
@@ -11,8 +11,8 @@ export function Transactional(isolationLevel?: IsolationLevel) {
     const originalMethod = descriptor.value;
 
     descriptor.value = async function (...args: any[]) {
-      
-      const newContext = Object.assign(this);
+
+      let newContext = Object.assign(this);
 
       const dataSourceKey = Object.keys(newContext)
         .find(key => newContext[key] instanceof DataSource)
@@ -23,13 +23,36 @@ export function Transactional(isolationLevel?: IsolationLevel) {
       await queryRunner.connect();
       await queryRunner.startTransaction(isolationLevel);
 
-      for (const key of Object.keys(newContext)) {
-        if (newContext[key] instanceof Repository) {
-          newContext[key] = queryRunner.manager.getRepository(
-            newContext[key].metadata.target,
-          );
+      const redefineContext = (context: any): any => {
+        for (const key of Object.keys(context)) {
+          let ctxObject = context[key];
+
+          if (Reflect.hasMetadata('self:properties_metadata', ctxObject?.constructor)) {
+            context[key] = redefineContext(
+              Object.assign(ctxObject)
+            );
+          } else if (ctxObject instanceof Repository) {
+            context[key] = queryRunner.manager.getRepository(
+              ctxObject.metadata.target,
+            );
+          }
         }
+        return context;
       }
+      newContext = redefineContext(newContext);
+
+      // for (const key of Object.keys(newContext)) {
+      //   let ctxObject = newContext[key];
+
+      //   if (Reflect.hasMetadata('self:properties_metadata', ctxObject?.constructor)) {
+      //     const copy = Object.assign(ctxObject);
+      //   }
+      //   if (ctxObject instanceof Repository) {
+      //     newContext[key] = queryRunner.manager.getRepository(
+      //       ctxObject.metadata.target,
+      //     );
+      //   }
+      // }
 
       try {
         const result = await originalMethod.apply(newContext, args);
@@ -37,6 +60,7 @@ export function Transactional(isolationLevel?: IsolationLevel) {
         return result;
       } catch (err) {
         await queryRunner.rollbackTransaction();
+        throw err;
       } finally {
         await queryRunner.release();
       }
