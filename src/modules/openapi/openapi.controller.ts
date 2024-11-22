797af9b33b17760f1815c62066c64140cabd5fd6
pendulum <rs.pendulum@gmail.com>
2024-11-22 20:07:27 +0300

    added new endpoints

--- src/modules/openapi/openapi.controller.ts
+++ src/modules/openapi/openapi.controller.ts
@@ -1,16 +1,120 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
-import { ApiBearerAuth, ApiHeader, ApiOkResponse, ApiOperation, ApiQuery, ApiTags } from '@nestjs/swagger';
+import { Controller, Get, Post, UseGuards, UseInterceptors } from '@nestjs/common';
+import { ApiBearerAuth, ApiBody, ApiConsumes, ApiHeader, ApiOkResponse, ApiOperation, ApiQuery, ApiTags } from '@nestjs/swagger';
 import { BaseGuard } from 'src/guards/base.guard';
 import { CONFIG_AUTH } from 'src/config/config.export';
 import { GetBalanceDtoReq, GetBalanceDtoRes, GetTransactionDtoReq, GetTransactionDtoRes } from './dto/openapi.dto';
 import { ApiErrorResponses } from 'src/swagger/errors-exception.dto';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller('vtb')
 @ApiTags('VTB Open API')
 export class OpenapiController {
 
+  @Post('crypto/ask/request')
+  @ApiHeader({
+    name: 'x-api-key',
+    description: 'секретный ключ приложения',
+    required: true,
+  })
+  @ApiHeader({
+    name: 'x-api-signature',
+    description: 'цифровая подпись на основе ECDSA',
+    required: true,
+  })
+  @ApiQuery({ type: GetBalanceDtoReq })
+  @ApiOkResponse({ type: GetBalanceDtoRes })
+  @ApiErrorResponses()
+  @ApiConsumes('multipart/form-data')
+  @UseInterceptors(FileInterceptor('file'))
+  public async cryptoAskRequest() {
+    return true;
+  }
+
+  @Post('crypto/ask/confirm')
+  @ApiHeader({
+    name: 'x-api-key',
+    description: 'секретный ключ приложения',
+    required: true,
+  })
+  @ApiHeader({
+    name: 'x-api-signature',
+    description: 'цифровая подпись на основе ECDSA',
+    required: true,
+  })
+  @ApiQuery({ type: GetBalanceDtoReq })
+  @ApiOkResponse({ type: GetBalanceDtoRes })
+  @ApiErrorResponses()
+  @ApiConsumes('multipart/form-data')
+  @UseInterceptors(FileInterceptor('file'))
+  public async cryptoAskConfirm() {
+    return true;
+  }
+
+  @Post('crypto/bid/request')
+  @ApiHeader({
+    name: 'x-api-key',
+    description: 'секретный ключ приложения',
+    required: true,
+  })
+  @ApiHeader({
+    name: 'x-api-signature',
+    description: 'цифровая подпись на основе ECDSA',
+    required: true,
+  })
+  @ApiQuery({ type: GetBalanceDtoReq })
+  @ApiOkResponse({ type: GetBalanceDtoRes })
+  @ApiErrorResponses()
+  @ApiConsumes('multipart/form-data')
+  @UseInterceptors(FileInterceptor('file'))
+  public async cryptoBidRequest() {
+    return true;
+  }
+
+  @Post('crypto/bid/confirm')
+  @ApiHeader({
+    name: 'x-api-key',
+    description: 'секретный ключ приложения',
+    required: true,
+  })
+  @ApiHeader({
+    name: 'x-api-signature',
+    description: 'цифровая подпись на основе ECDSA',
+    required: true,
+  })
+  @ApiBody({ type: GetBalanceDtoReq })
+  @ApiOkResponse({ type: GetBalanceDtoRes })
+  @ApiErrorResponses()
+  @ApiConsumes('multipart/form-data')
+  @UseInterceptors(FileInterceptor('file'))
+  public async cryptoBidConfirm() {
+    return true;
+  }
+
+  @Post('crypto/prepaid')
+  @ApiHeader({
+    name: 'x-api-key',
+    description: 'секретный ключ приложения',
+    required: true,
+  })
+  @ApiHeader({
+    name: 'x-api-signature',
+    description: 'цифровая подпись на основе ECDSA',
+    required: true,
+  })
+  @ApiQuery({ type: GetBalanceDtoReq })
+  @ApiOkResponse({ type: GetBalanceDtoRes })
+  @ApiErrorResponses()
+  @ApiConsumes('multipart/form-data')
+  @UseInterceptors(FileInterceptor('file'))
+  public async cryptoPrepaid() {
+    return true;
+  }
+  
   @Get('balance')
-  @ApiOperation({ summary: 'Запрос возвращает доступный баланс по продукту'})
+  @ApiOperation({ 
+    summary: 'Запрос возвращает доступный баланс по продукту',
+    deprecated: true,
+  })
   @ApiHeader({
     name: 'x-api-key',
     description: 'секретный ключ приложения',
@@ -29,7 +133,10 @@ export class OpenapiController {
   }
 
   @Get('transaction')
-  @ApiOperation({ summary: 'Запрос возвращает историю транзакций за период по продукту'})
+  @ApiOperation({ 
+    deprecated: true, 
+    summary: 'Запрос возвращает историю транзакций за период по продукту'
+  })
   @ApiHeader({
     name: 'x-api-key',
     description: 'секретный ключ приложения',
@@ -46,5 +153,4 @@ export class OpenapiController {
   public async getTransactions() {
     return true;
   }
-
 }
